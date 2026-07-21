71aec9a6c1a4f0940625878e9947e68b632a9e83
joemoorhouse <joe.moorhouse@gmail.com>
2026-07-21 19:37:37 +0100

    First attempt insurance model
    Signed-off-by: joemoorhouse <joe.moorhouse@gmail.com>

---
+++ src/physrisk/kernel/insurance_model.py
@@ -0,0 +1,53 @@
+
+
+from typing import NamedTuple
+
+import numpy as np
+from typing_extensions import Protocol
+
+from physrisk.kernel.assets import Asset
+from physrisk.kernel.financial_model import FinancialDataProvider
+from physrisk.kernel.hazards import Hazard
+
+
+class SectoralInsuranceData(NamedTuple):
+    uptake: float # statistical insurance uptake: probability that asset in sector is insured
+    deductable: float # deductable as a fraction of total insured value
+    limit: float # limit as a fraction of total insured value
+
+
+class InsuranceDataProvider(Protocol):
+    def __call__(self, asset: Asset, hazard_type: type[Hazard], impact_type: str) -> SectoralInsuranceData:
+        """Provide insurance data, including state support.
+
+        Args:
+            asset (Asset): Asset
+            hazard_type (type[Hazard]): Hazard type
+            impact_type (str): `damage` or `disruption`
+        """
+        ...
+
+
+class InsuranceModel(Protocol):
+    """ "Insurance Model using a FinancialDataProvider as source of information."""
+
+    @property
+    def financial_data_provider(self) -> FinancialDataProvider:
+        """Get the financial data provider."""
+
+    def claims_payment_from_restoration_cost(
+        self, sectoral_insurance_data: SectoralInsuranceData, restoration_cost: np.ndarray
+    ) -> tuple[np.ndarray, np.ndarray]:
+        """Convert damage, specified as a fraction of total insurable value, to cost
+        of asset restoration and loss of revenue during downtime.
+
+        Args:
+            asset (Asset): Asset.
+            impact (np.ndarray): Damage as a fraction of total insurable value.
+            currency (str): Currency (3-letter code).
+
+        Returns:
+            tuple[np.ndarray, np.ndarray]: Tuple containing:
+                - Cost of asset restoration in specified currency.
+                - Annual loss of revenue from downtime in specified currency.
+        """
